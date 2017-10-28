86a1e1d074d858cfc7f2417a645b9813d1232815
Branford Harris <branfordharris@Branfords-MacBook-Pro.local>
2017-10-28 13:01:01 -0500

    fixed firebase.data() to firebase.database();

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
   
-  var database = firebase.data();
+  var database = firebase.database();
 
 
 	$("#submit").on("click", function(event){
